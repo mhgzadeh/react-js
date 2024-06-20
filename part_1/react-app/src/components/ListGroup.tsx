a584f5280cdec819558ae15589ee6d1c02f5653d
Mohammad <m.hgzadeh@gmail.com>
2024-06-20 08:06:34 +0200

    add useState

--- part_1/react-app/src/components/ListGroup.tsx
+++ part_1/react-app/src/components/ListGroup.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Tehran", "Berlin", "Stuttgart", "Londen"];
-  items = [];
+  // Hock
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h1>List Items</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
