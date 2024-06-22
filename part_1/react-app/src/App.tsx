271eecad1c050d0e3802bbba9129c890fe140b1d
Mohammad <m.hgzadeh@gmail.com>
2024-06-22 15:55:04 +0200

    add children

--- part_1/react-app/src/App.tsx
+++ part_1/react-app/src/App.tsx
@@ -1,14 +1,25 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "Berlin", "Tehran", "Tokyo"]
+  let items = ["New York", "Berlin", "Tehran", "Tokyo"];
   const handleSelectItem = (item: string) => {
-    console.log(item); 
-  }
+    console.log(item);
+  };
   return (
-    <div>
-      <ListGroup items={items} headings="Cities" onSelectItem={handleSelectItem}/>
-    </div>
+    <>
+      <div>
+        <ListGroup
+          items={items}
+          headings="Cities"
+          onSelectItem={handleSelectItem}
+        />
+        <br />
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+      </div>
+    </>
   );
 }
 
