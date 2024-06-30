b3a2de62f0ab7829c87e18bbacecf10ecb49bd00
Mohammad <m.hgzadeh@gmail.com>
2024-06-30 15:57:45 +0200

    zod

--- part_2/handeling_forms/src/components/Form.tsx
+++ part_2/handeling_forms/src/components/Form.tsx
@@ -1,20 +1,25 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(3, { message: "Min 3 character is needed" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be min 18." }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
-    console.log(errors);
   };
 
   return (
@@ -24,16 +29,13 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger ms-3 mt-2">the name field is empty.</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger ms-3 mt-2">it needs more than 3 character</p>
+        {errors.name && (
+          <p className="text-danger ms-3 mt-2">{errors.name.message}</p>
         )}
       </div>
       <div className="mb-3">
@@ -41,13 +43,18 @@ const Form = () => {
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && (
+          <p className="text-danger ms-3 mt-2">{errors.age.message}</p>
+        )}
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button disabled={!isValid} className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 };
---
+++ part_2/handeling_forms/src/components/FormValidation.tsx
@@ -0,0 +1,55 @@
+import { FieldValues, useForm } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    console.log(errors);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="foem-label">
+          Name
+        </label>
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && (
+          <p className="text-danger ms-3 mt-2">the name field is empty.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger ms-3 mt-2">it needs more than 3 character</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
