6d4c2d414f99fd222e8c2299666f45d11995fe7f
Mohammad <m.hgzadeh@gmail.com>
2024-06-30 11:57:19 +0200

    control components using useState

--- part_2/handeling_forms/src/components/Form.tsx
+++ part_2/handeling_forms/src/components/Form.tsx
@@ -1,23 +1,14 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: '', age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handelSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log(
-      `name: ${nameRef.current?.value}, age: ${ageRef.current?.value}`
-    );
-    if (nameRef.current !== null) {
-      person.name = nameRef.current.value;
-    }
-    if (ageRef.current !== null) {
-      person.age = parseInt(ageRef.current.value)
-    }
     console.log(person);
-    
   };
 
   return (
@@ -26,13 +17,29 @@ const Form = () => {
         <label htmlFor="name" className="foem-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          value={person.name}
+          onChange={(event) => {
+            setPerson({ ...person, name: event.target.value });
+          }}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          value={person.age}
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
---
+++ part_2/handeling_forms/src/components/FormUseRef.tsx
@@ -0,0 +1,44 @@
+'using useRef to access input elements'
+
+import { FormEvent, useRef } from "react";
+
+const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: '', age: 0 };
+
+  const handelSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(
+      `name: ${nameRef.current?.value}, age: ${ageRef.current?.value}`
+    );
+    if (nameRef.current !== null) {
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      person.age = parseInt(ageRef.current.value)
+    }
+    console.log(person);
+    
+  };
+
+  return (
+    <form onSubmit={handelSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="foem-label">
+          Name
+        </label>
+        <input ref={nameRef} id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input ref={ageRef} id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
