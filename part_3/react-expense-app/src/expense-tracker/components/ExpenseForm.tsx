8de9c9314d9629a9124254eb09d16e7bd7a069cb
Mohammad <m.hgzadeh@gmail.com>
2024-07-06 22:39:24 +0200

    recode category error message

--- part_3/react-expense-app/src/expense-tracker/components/ExpenseForm.tsx
+++ part_3/react-expense-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -12,9 +12,10 @@ const schema = z.object({
     .number({ invalid_type_error: "Amount is required" })
     .min(0.01)
     .max(100_000),
-  category: z.enum(categories, {
-    errorMap: () => ({ message: "Category is required" }),
-  }),
+  // category: z.enum(categories, {
+  //   errorMap: () => ({ message: "Category is required" }),
+  // }),
+  category: z.enum(categories, { message: "Category is required" }),
 });
 
 type ExpenseFormData = z.infer<typeof schema>;
