94c7fc2d1ba952ba2b48b40d02cb7a05271fbf63
Mohammad <m.hgzadeh@gmail.com>
2024-07-06 22:33:37 +0200

    update form validation

--- part_3/react-expense-app/src/expense-tracker/components/ExpenseForm.tsx
+++ part_3/react-expense-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -1,19 +1,60 @@
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import categories from "../categories";
 
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: "Description should be min 3 character." })
+    .max(50),
+  amount: z
+    .number({ invalid_type_error: "Amount is required" })
+    .min(0.01)
+    .max(100_000),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: "Category is required" }),
+  }),
+});
+
+type ExpenseFormData = z.infer<typeof schema>;
+
 const ExpenseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+
   return (
-    <form className="mb-5">
+    <form className="mb-5" onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input type="text" id="description" className="form-control" />
+        <input
+          {...register("description")}
+          type="text"
+          id="description"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input type="number" id="amount" className="form-control" />
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          type="number"
+          id="amount"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
@@ -27,6 +68,9 @@ const ExpenseForm = () => {
             </option>
           ))}
         </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
