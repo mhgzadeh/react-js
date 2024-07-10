1cb3ca330c12ee7c71b660cad84e510ee54273ba
Mohammad <m.hgzadeh@gmail.com>
2024-07-10 22:51:31 +0200

    error handeling + add loader

--- part_4/react-use-effect/src/App.tsx
+++ part_4/react-use-effect/src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useState, useEffect } from "react";
 
 interface User {
@@ -9,15 +9,34 @@ interface User {
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/userss")
-      .then((response) => setUsers(response.data))
-      .catch((err) => setError(err.message));
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((response) => {
+        setUsers(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        // if the error is the of cancel error then return nothing
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+    // .finally(() => {
+    //   setIsLoading(false)
+    //  });
+    return () => controller.abort();
   }, []);
   return (
     <div className="container mt-5">
+      {isLoading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map((user) => (
