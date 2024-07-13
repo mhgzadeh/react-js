19b1aab006087e8dd6552078a1f2c10a23ed7d28
Mohammad <m.hgzadeh@gmail.com>
2024-07-13 23:23:39 +0200

    navbar

--- part_5/game-hub/src/App.tsx
+++ part_5/game-hub/src/App.tsx
@@ -1,12 +1,19 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid templateAreas={`"nav nav" "aside main"`}>
-      <GridItem area={"nav"} bg={"coral"}>
-        Nav
+    <Grid
+      mx={20}
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
       </GridItem>
-      <Show>
+      <Show above="lg">
         <GridItem area={"aside"} bg={"gold"}>
           Aside
         </GridItem>
