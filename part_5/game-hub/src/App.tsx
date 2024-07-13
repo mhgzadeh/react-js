3edd3c9e70e44d0e12171afccbd8d02230907520
Mohammad <m.hgzadeh@gmail.com>
2024-07-14 00:36:36 +0200

    add container

--- part_5/game-hub/src/App.tsx
+++ part_5/game-hub/src/App.tsx
@@ -1,27 +1,30 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Container, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid
-      mx={20}
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+    <Container
+      maxW={{
+        base: "100%",
+        md: "90%",
+        lg: "80%",
       }}
     >
-      <GridItem area={"nav"}>
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area={"aside"} bg={"gold"}>
-          Aside
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"}>
+          <NavBar />
         </GridItem>
-      </Show>
-      <GridItem area={"main"} bg={"dodgerblue"}>
-        Main
-      </GridItem>
-    </Grid>
+        <Show above="lg">
+          <GridItem area={"aside"}>Aside</GridItem>
+        </Show>
+        <GridItem area={"main"}>Main</GridItem>
+      </Grid>
+    </Container>
   );
 }
 
