b31a7c80e07357507af3316ab2f59fbb4c290f15
Mohammad <m.hgzadeh@gmail.com>
2024-07-14 23:26:35 +0200

    add platform icon list

--- part_5/game-hub/src/hooks/useGame.tsx
+++ part_5/game-hub/src/hooks/useGame.tsx
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGameResponse {
@@ -20,7 +27,7 @@ const useGame = () => {
     const controller = new AbortController();
 
     apiClient
-      .get<FetchGameResponse>("/games", {signal: controller.signal})
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
       })
