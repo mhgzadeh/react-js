562952f1e97e424a4cc29d4ce58d33c578079ab2
Mohammad <m.hgzadeh@gmail.com>
2024-07-14 15:44:08 +0200

    add controller to useGame hooks

--- part_5/game-hub/src/components/GameGrid.tsx
+++ part_5/game-hub/src/components/GameGrid.tsx
@@ -1,37 +1,14 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { ListItem, Text, UnorderedList } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGame from "../hooks/useGame";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGameResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((err: Error) => setError(err.message));
-  }, []);
-
+  const { games, error } = useGame();
   return (
     <>
       {error && <Text color="red">{error}</Text>}
       <UnorderedList>
         {games.map((game) => (
           <ListItem key={game.id}>{game.name}</ListItem>
-          
         ))}
       </UnorderedList>
     </>
