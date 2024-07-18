fe8ad833a5c0cb25bb8cd0661a1bdc65b854bed6
Mohammad <m.hgzadeh@gmail.com>
2024-07-18 22:28:23 +0200

    init criticscard

--- part_5/game-hub/src/components/GameCard.tsx
+++ part_5/game-hub/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGame";
 import PlatfomIconList from "./PlatfomIconList";
+import CriticsCard from "./CriticsCard";
 
 interface Props {
   game: Game;
@@ -8,11 +9,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
+    <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize='2l' mb={3}>{game.name}</Heading>
-        <PlatfomIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+        <Heading fontSize="2l" mb={3}>
+          {game.name}
+        </Heading>
+        <HStack justifyContent={"space-between"}>
+          <PlatfomIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticsCard score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
