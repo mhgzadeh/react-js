a8944ccb75b9ddf048db93ce6bb030cdc32917e9
Mohammad <m.hgzadeh@gmail.com>
2024-07-20 00:30:39 +0200

    remove unuse react components and some comments

--- part_5/game-hub/src/AppWithSampleGrid.tsx
+++ part_5/game-hub/src/AppWithSampleGrid.tsx
@@ -1,33 +1,7 @@
 import { Container, Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
-    // <Container
-    //   maxW={{
-    //     base: "100%",
-    //     md: "90%",
-    //     lg: "80%",
-    //   }}
-    // >
-    //   <Grid
-    //     templateAreas={{
-    //       base: `"nav" "main"`,
-    //       lg: `"nav nav" "aside main"`,
-    //     }}
-    //   >
-    //     <GridItem area={"nav"}>
-    //       <NavBar />
-    //     </GridItem>
-    //     <Show above="lg">
-    //       <GridItem area={"aside"}>Aside</GridItem>
-    //     </Show>
-    //     <GridItem area={"main"}>
-    //       <GameGrid />
-    //     </GridItem>
-    //   </Grid>
-    // </Container>
     <Container maxW={"8xl"}>
       <Grid
         h="800px"
