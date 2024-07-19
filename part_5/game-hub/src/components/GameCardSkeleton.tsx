daac3bbf3bab91799a9207789f5eec8ad9dc84b6
Mohammad <m.hgzadeh@gmail.com>
2024-07-20 00:42:00 +0200

    remove card width

--- part_5/game-hub/src/components/GameCardSkeleton.tsx
+++ part_5/game-hub/src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width={'300px'}>
+    <Card>
       <Skeleton height={'300px'} borderRadius={10} overflow="hidden">
         <CardBody>
           <SkeletonText />
