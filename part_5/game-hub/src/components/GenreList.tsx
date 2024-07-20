3be69adfac04d32320457c69ae3667f7a75df86c
Mohammad <m.hgzadeh@gmail.com>
2024-07-20 12:35:30 +0200

    display genre

--- part_5/game-hub/src/components/GenreList.tsx
+++ part_5/game-hub/src/components/GenreList.tsx
@@ -1,12 +1,23 @@
-import { List, ListItem } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenre();
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id}>{genre.name}</ListItem>
+        <ListItem key={genre.id}>
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize={20}
+              borderRadius={5}
+              pb={3}
+            />
+            <Text ms={2}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
     </List>
   );
