3a5d05b3b8ee72901696766cdcb0ce4d11cbf71d
Mohammad <m.hgzadeh@gmail.com>
2024-07-22 21:50:24 +0200

    Build search input

--- part_5/game-hub/src/components/ColorModeSwitch.tsx
+++ part_5/game-hub/src/components/ColorModeSwitch.tsx
@@ -1,11 +1,17 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Show, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
-      <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode} size="md" />
-      <Text>Dark Mode</Text>
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        size="md"
+      />
+      <Show above="lg">
+        <Text whiteSpace={"nowrap"}>Dark Mode</Text>
+      </Show>
     </HStack>
   );
 };
--- part_5/game-hub/src/components/NavBar.tsx
+++ part_5/game-hub/src/components/NavBar.tsx
@@ -1,14 +1,13 @@
 import { Center, HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack justifyContent={"space-between"}>
-      <Center>
-        <Image src={logo} boxSize={20} />
-        <Text>NavBar</Text>
-      </Center>
+      <Image src={logo} boxSize={20} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
