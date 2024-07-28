2a175a9f4bf3200b1ceeb6824b510fbf0bfad33c
Mohammad <m.hgzadeh@gmail.com>
2024-07-28 21:52:15 +0200

    handeling mutation error

--- part_6/react-query/src/components/TodoForm.tsx
+++ part_6/react-query/src/components/TodoForm.tsx
@@ -1,18 +1,23 @@
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import {
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useRef } from "react";
 import { Todo } from "../hooks/useTodos";
 
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
-        .post("https://jsonplaceholder.typicode.com/todos", todo)
+        .post("https://jsonplaceholder.typicode.com/xtodos", todo)
         .then((response) => response.data),
     onSuccess: (savedTodos) => {
       // APPROACH: Invalidating the cache
@@ -28,27 +33,35 @@ const TodoForm = () => {
 
   const ref = useRef<HTMLInputElement>(null);
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current && ref.current.value) {
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            completed: false,
-            userId: 1,
-          });
-        }
-      }}
-    >
-      <FormControl mb={3}>
-        <FormLabel>New Todos</FormLabel>
-        <Input ref={ref} type="text" />
-        <Button mt={2} colorScheme="teal" type="submit" size="sm">
-          Submit
-        </Button>
-      </FormControl>
-    </form>
+    <>
+      {addTodo.error && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>{addTodo.error.message}</AlertTitle>
+        </Alert>
+      )}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (ref.current && ref.current.value) {
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            });
+          }
+        }}
+      >
+        <FormControl mb={3}>
+          <FormLabel>New Todos</FormLabel>
+          <Input ref={ref} type="text" />
+          <Button mt={2} colorScheme="teal" type="submit" size="sm">
+            Submit
+          </Button>
+        </FormControl>
+      </form>
+    </>
   );
 };
 
