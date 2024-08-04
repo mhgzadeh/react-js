c542cb59ce11f8a1c0e20be9e8494f53177d6aa7
Mohammad <m.hgzadeh@gmail.com>
2024-08-04 11:14:13 +0200

    refotmat login status

--- part_8/react-query/src/state-management/components/Loginstatus.tsx
+++ part_8/react-query/src/state-management/components/Loginstatus.tsx
@@ -7,12 +7,22 @@ const Loginstatus = () => {
   if (user)
     return (
       <HStack>
-        <Button onClick={() => dispatch({ type: "LOGOUT" })} colorScheme={'blue'}>Logout</Button>
+        <Button
+          onClick={() => dispatch({ type: "LOGOUT" })}
+          colorScheme={"blue"}
+          mb={2}
+        >
+          Logout
+        </Button>
         <Text>{user}</Text>
       </HStack>
     );
   return (
-    <Button onClick={() => dispatch({ type: "LOGIN", username: "Mohammad" })} colorScheme="red">
+    <Button
+      onClick={() => dispatch({ type: "LOGIN", username: "Mohammad" })}
+      colorScheme="red"
+      mb={2}
+    >
       Login
     </Button>
   );
