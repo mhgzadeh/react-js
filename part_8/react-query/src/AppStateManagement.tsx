f3c7a9536838d5d62f2251207a1c5ddd8cf8c9bb
Mohammad <m.hgzadeh@gmail.com>
2024-08-08 21:46:12 +0200

    init react router

---
+++ part_8/react-query/src/AppStateManagement.tsx
@@ -0,0 +1,28 @@
+import { Container, Grid, GridItem, Text } from "@chakra-ui/react";
+import Counter from "./state-management/counter/Counter";
+import NavBar from "./state-management/components/NavBar";
+import { TasksProvider, TaskList } from "./state-management/tasks";
+
+function App() {
+  return (
+    <Container maxW={"10xl"}>
+      <Grid templateColumns="repeat(2, 1fr)" columnGap={5}>
+        <GridItem mt={10}>
+          <Text fontWeight={"bolder"}>Reducer</Text>
+          <Counter />
+        </GridItem>
+        <GridItem mt={10}>
+          <Text fontWeight={"bolder"} mb={2}>
+            NavBar
+          </Text>
+          <TasksProvider>
+            <NavBar />
+            <TaskList />
+          </TasksProvider>
+        </GridItem>
+      </Grid>
+    </Container>
+  );
+}
+
+export default App;
