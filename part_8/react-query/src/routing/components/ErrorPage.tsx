e2ae9d086b4ee71f3a42f89b63a9338630cc180e
Mohammad <m.hgzadeh@gmail.com>
2024-08-10 14:18:20 +0200

    Handeling error

--- part_8/react-query/src/routing/components/ErrorPage.tsx
+++ part_8/react-query/src/routing/components/ErrorPage.tsx
@@ -1,9 +1,16 @@
+import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+
   return (
-    <>
+    <div className="container mt-3">
       <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </>
+      <p>{isRouteErrorResponse(error)? error.statusText : 'Sorry, an unexpected error has occurred.'}</p>
+      <Link to={"/"} className="btn btn-warning">
+        Back to the main page
+      </Link>
+    </div>
   );
 };
 
--- part_8/react-query/src/routing/rootes.tsx
+++ part_8/react-query/src/routing/rootes.tsx
@@ -4,11 +4,13 @@ import Layout from "./components/Layout";
 import UsersPage from "./components/UsersPage";
 import ContactPage from "./components/ContactPage";
 import UserDetail from "./components/UserDetail";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       {
